44a695d2a17e9ed8a8401eb12d276df435f86945
Anne <an-san@gmx.de>
2026-01-21 10:18:41 +0100

    feat: add color palette for the option to change the color of the webside

---
+++ src/app/shared/color-palette.ts
@@ -0,0 +1,22 @@
+
+type ColorPalette = {
+    primaryDarkColor: string,
+    primaryColor: string,
+};
+
+const colorPaletteOp1: ColorPalette = {
+    primaryDarkColor: "#262E34",
+    primaryColor: "#89BCD9",
+}
+
+const colorPaletteOp2: ColorPalette = {
+    primaryDarkColor: "#042639",
+    primaryColor: "#F28A62",
+}
+
+const colorPaletteOp3: ColorPalette = {
+    primaryDarkColor: "#0A383F",
+    primaryColor: "#F9CB71",
+}
+
+export const colorPalette: ColorPalette = colorPaletteOp1;
