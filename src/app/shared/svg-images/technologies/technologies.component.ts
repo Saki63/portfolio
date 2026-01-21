c72deae7ec9367d27f98cab325540bb8c43e5765
Anne <an-san@gmx.de>
2026-01-21 10:17:30 +0100

    feat: add technology icons as svg components

---
+++ src/app/shared/svg-images/technologies/technologies.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { BaseImageComponent } from '../base-image.component';
+
+@Component({
+  selector: 'app-icon-angular',
+  templateUrl: './svgs/angular-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconAngularComponent extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-typescript',
+  templateUrl: './svgs/typescript-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconTypeScript extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-html',
+  templateUrl: './svgs/html-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconHTML extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-css',
+  templateUrl: './svgs/css-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconCSS extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-javascript',
+  templateUrl: './svgs/javascript-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconJavaScript extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-firebase',
+  templateUrl: './svgs/firebase-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconFirebase extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-material-design',
+  templateUrl: './svgs/material-design-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconMaterialDesign extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-scrum',
+  templateUrl: './svgs/scrum-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconScrum extends BaseImageComponent {}
+
+@Component({
+  selector: 'app-icon-rest-api',
+  templateUrl: './svgs/rest-api-icon.component.html',
+  styleUrl: './technologies.component.scss'
+})
+export class IconRestApi extends BaseImageComponent {}
