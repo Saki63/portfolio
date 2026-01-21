6854dbf08060b5bb013d8081b666f90b965c4002
Anne <an-san@gmx.de>
2026-01-21 10:18:08 +0100

    feat: change technology svgs in my-skills section to components

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SvgImageModule } from '../shared/svg-images/svg-images';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [SvgImageModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+  isSmallScreen = window.innerWidth <= 480;
+  iconSize = 60;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isSmallScreen = window.innerWidth <= 480;
+
+    this.iconSize = this.isSmallScreen ? 45 : 60;
+  }
 }
