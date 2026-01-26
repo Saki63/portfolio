af00f0f942c88a8af83f7329f2b1723f1ab65e38
Anne <an-san@gmx.de>
2026-01-26 18:39:27 +0100

    feat: add global color palette to why me section

--- src/app/why-me/why-me.component.ts
+++ src/app/why-me/why-me.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SvgImageModule } from '../shared/svg-images/svg-images';
+import { colorPalette } from '../shared/color-palette';
 
 @Component({
   selector: 'app-why-me',
@@ -9,6 +10,6 @@ import { SvgImageModule } from '../shared/svg-images/svg-images';
   styleUrl: './why-me.component.scss'
 })
 export class WhyMeComponent {
-
+  colorPalette = colorPalette;
 }
 
