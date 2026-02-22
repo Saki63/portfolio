98e66fdefbdd1476a4e9350732ed45d1634ddece
Anne <an-san@gmx.de>
2026-02-22 10:26:50 +0100

    feat: scroll behavior to quote in bubbles

--- src/app/references/bubble/bubble.component.ts
+++ src/app/references/bubble/bubble.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { SvgImageModule } from '../../shared/svg-images/svg-images';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-bubble',
   standalone: true,
-  imports: [ SvgImageModule ],
+  imports: [SvgImageModule, NgClass],
   templateUrl: './bubble.component.html',
   styleUrl: './bubble.component.scss'
 })
@@ -13,5 +14,17 @@ export class BubbleComponent {
   @Input() project: string = "";
   @Input() quote: string = "";
   @Input() link: string = "";
+  @Input() scrollbehavior: boolean = false;
+
+  scrollable = true;
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight - 1) {
+      this.scrollable = false;
+    } else {
+      this.scrollable = true;
+    }
+  }
 
 }
