99618f6664d52d0a9f91c393c69d04f8300f50fb
Anne <an-san@gmx.de>
2026-02-24 16:36:39 +0100

    feat: adjust responsive menu

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -4,11 +4,12 @@ import { SvgImageModule } from '../shared/svg-images/svg-images';
 import { colorPalette } from '../shared/color-palette';
 import { translation } from '../shared/translation';
 import { LanguageSelectionComponent } from "../shared/components/language-selection/language-selection.component";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [LogoComponent, SvgImageModule, LanguageSelectionComponent],
+  imports: [LogoComponent, SvgImageModule, LanguageSelectionComponent, NgClass],
   templateUrl: './landingpage.component.html',
   styleUrls: ['../../styles.scss', './landingpage.component.scss']
 })
@@ -23,17 +24,28 @@ export class LandingpageComponent {
   }
   
   hoverPic = false;
+  isMenuHiding = true;
 
   isSmallScreen = window.innerWidth <= 560 ? 'smallest' : (window.innerWidth <= 1050 ? 'small' : 'normal');
 
   ellipse1Height = this.isSmallScreen === 'smallest' ? 320 : (this.isSmallScreen === 'small' ? 513 : 641);
   ellipse1Width = this.isSmallScreen === 'smallest' ? 290 : (this.isSmallScreen === 'small' ? 464 : 580);
   
+  ellipse2Height = this.isSmallScreen === 'smallest' ? 320 : 370;
+  ellipse2Width = this.isSmallScreen === 'smallest' ? 290 : 353;
+
   @HostListener('window:resize')
   onResize() {
     this.isSmallScreen = window.innerWidth <= 560 ? 'smallest' : (window.innerWidth <= 1050 ? 'small' : 'normal');
 
     this.ellipse1Height = this.isSmallScreen === 'smallest' ? 310 : (this.isSmallScreen === 'small' ? 513 : 641);
     this.ellipse1Width = this.isSmallScreen === 'smallest' ? 280 : (this.isSmallScreen === 'small' ? 464 : 580);
+
+    this.ellipse2Height = this.isSmallScreen === 'smallest' ? 320 : 370;
+    this.ellipse2Width = this.isSmallScreen === 'smallest' ? 290 : 353;
+  }
+
+  toggleMenu(){
+    this.isMenuHiding = this.isMenuHiding ? false : true;
   }
 }
