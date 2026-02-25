c5a9658306a431f246bbac307353932ae03056d6
Anne <an-san@gmx.de>
2026-02-25 20:51:55 +0100

    feat: change routes for multiple pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'legal-notice',
+        component: LegalNoticeComponent,
+    },
+    {
+        path: '',
+        component: PrivacyPolicyComponent,
+    },
+];
