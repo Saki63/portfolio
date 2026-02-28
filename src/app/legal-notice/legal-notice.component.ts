64193a94ecd2e2b0935e29612eaf3b6a7feed5c9
Anne <an-san@gmx.de>
2026-02-28 17:46:55 +0100

    feat: add legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,18 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NavigationBarComponent } from "../shared/components/navigation-bar/navigation-bar.component";
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { NavigationBarComponent } from '../shared/components/navigation-bar/navigation-bar.component';
+import { LanguageService } from '../shared/language-service'
 import { translation } from '../shared/translation';
-import { LanguageService } from '../shared/language-service';
+import { LogoComponent } from '../shared/logo/logo.component';
+import { colorPalette } from '../shared/color-palette';
+import { SvgImageModule } from '../shared/svg-images/svg-images';
+import { NgClass } from '@angular/common';
+import { LanguageSelectionComponent } from '../shared/components/language-selection/language-selection.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [NavigationBarComponent],
+  imports: [NavigationBarComponent, LogoComponent, SvgImageModule, NgClass, LanguageSelectionComponent, RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+
+  colorPalette = colorPalette;
   language = 'de';
-  translation = translation;
+  legalNoticeTranslation = translation['page']['legal-notice'];
+  translationMenu = translation;
+  isMenuHiding = true;
+  isSmallScreen = window.innerWidth <= 560 ? 'smallest' : (window.innerWidth <= 1050 ? 'small' : 'normal');
+  ellipse2Height = this.isSmallScreen === 'smallest' ? 320 : 370;
+  ellipse2Width = this.isSmallScreen === 'smallest' ? 290 : 353;
 
   constructor(private langService: LanguageService) {}
 
@@ -20,5 +33,17 @@ export class LegalNoticeComponent {
     this.langService.language$.subscribe(lang => {
       this.language = lang;
     });
+  }
+
+  toggleMenu(){
+    this.isMenuHiding = this.isMenuHiding ? false : true;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isSmallScreen = window.innerWidth <= 560 ? 'smallest' : (window.innerWidth <= 1050 ? 'small' : 'normal');
+
+    this.ellipse2Height = this.isSmallScreen === 'smallest' ? 320 : 370;
+    this.ellipse2Width = this.isSmallScreen === 'smallest' ? 290 : 353;
   }
 }
--- src/app/shared/translation.ts
+++ src/app/shared/translation.ts
@@ -469,6 +469,86 @@ export const translation = {
                 }
             }
             
+        },
+        'legal-notice': {
+            'title': {
+                en: "Legal notice",
+                de: "Rechtlicher Hinweis"
+            }, 
+            'imprint': {
+                'title': {
+                    en: "Imprint",
+                    de: "Impressum",
+                },
+                'information': {
+                    en: "Information pursuant to § 5 DDG",
+                    de: "Angaben gemäß § 5 DDG",
+                },
+                'represent': {
+                    en: "Represented by:",
+                    de: "Vertreten durch:",
+                },
+                'contact': {
+                    en: "Contact:",
+                    de: "Kontakt:",
+                },
+                'telephone': {
+                    en: "Telephone:",
+                    de: "Telefon:",
+                },
+                'email': {
+                    en: "Email:",
+                    de: "E-Mail:",
+                },
+                'responsibility': {
+                    en: "Responsible for content in accordance with § 18 para. 2 MStV:",
+                    de: "Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV:",
+                },
+                'disclaimer': {
+                    en: "Disclaimer:",
+                    de: "Haftungsausschluss:",
+                },
+                'liability-content': {
+                    en: "Liability for content",
+                    de: "Haftung für Inhalte",
+                },
+                'liability-content-text': {
+                    en: "The content of our pages has been compiled with the utmost care. However, we cannot guarantee the accuracy, completeness, or timeliness of the content. As a service provider, we are responsible for our own content on these pages in accordance with § 7 para. 1 DDG (German Telemedia Act) under general law. However, according to §§ 8 to 10 DDG, we as a service provider are not obliged to monitor transmitted or stored third-party information or to investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information in accordance with general laws remain unaffected by this. However, liability in this regard is only possible from the time we become aware of a specific legal violation. If we become aware of such legal violations, we will remove this content immediately.",
+                    de: "Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 DDG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 DDG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.",
+                },
+                'liability-links': {
+                    en: "Liability for links",
+                    de: "Haftung für Links",
+                },
+                'liability-links-text': {
+                    en: "Our website contains links to external third-party websites over whose content we have no influence. Therefore, we cannot accept any liability for this external content. The respective provider or operator of the linked pages is always responsible for their content. The linked pages were checked for possible legal violations at the time of linking. No illegal content was found at the time of linking. However, permanent monitoring of the content of the linked pages is not reasonable without concrete evidence of a violation of the law. If we become aware of any violations of the law, we will remove such links immediately.",
+                    de: "Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.",
+                },
+                'copyright-title': {
+                    en: "Copyright",
+                    de: "Urheberrecht",
+                },
+                'copyright-text': {
+                    en: "The content and works created by the website operators on these pages are subject to German copyright law. Reproduction, processing, distribution, and any form of exploitation beyond the limits of copyright law require the written consent of the respective author or creator. Downloads and copies of this page are permitted only for private, non-commercial use. Insofar as the content on this page was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is identified as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. Upon notification of legal violations, we will remove such content immediately.",
+                    de: "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.",
+                },
+                'imprint-from': {
+                    en: "Imprint from",
+                    de: "Impressum von",
+                },
+                'privacy-policy': {
+                    en: "Privacy Policy",
+                    de: "Datenschutz",
+                },
+                'privacy-policy-link-text': {
+                    en: "Click here for the",
+                    de: "Hier geht es zum ",
+                },
+                'privacy-policy-link': {
+                    en: "privacy policy",
+                    de: "Datenschutz",
+                },
+            }
         }
     }
 }
