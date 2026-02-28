3e2221f75f67c59e53ed229e1a3c5712c3fcddbe
Anne <an-san@gmx.de>
2026-02-28 14:58:03 +0100

    refactor: delete unnecessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingpageComponent } from "./landingpage/landingpage.component";
-import { SectionComponent } from "./section/section.component";
-import { WhyMeComponent } from "./why-me/why-me.component";
-import { MySkillsComponent } from './my-skills/my-skills.component';
-import { MyProjectsComponent } from './my-projects/my-projects.component';
-import { ReferencesComponent } from './references/references.component';
-import { ContactMeComponent } from "./contact-me/contact-me.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
-import { NavigationBarComponent } from "./shared/components/navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingpageComponent, SectionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, ReferencesComponent, ContactMeComponent, FooterComponent, NavigationBarComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
