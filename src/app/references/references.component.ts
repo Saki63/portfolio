1e361eaff8117195b78b3e3d4f610c34f209e675
Anne <an-san@gmx.de>
2026-03-04 08:26:28 +0100

    fix: add scroll information to bubbles

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { BubbleComponent } from "./bubble/bubble.component";
 import { translation } from '../shared/translation';
+import { SvgImageModule } from "../shared/svg-images/svg-images";
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [BubbleComponent],
+  imports: [BubbleComponent, SvgImageModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
   translation = translation;
   @Input() language = 'en';
+
+  scrollbehavior = window.innerWidth <= 858;
+
+  scrollable = true;
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+    if (event.target.offsetWidth + event.target.scrollLeft >= event.target.scrollWidth - 1) {
+      this.scrollable = false;
+    } else {
+      this.scrollable = true;
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.scrollbehavior = window.innerWidth <= 858;
+  }
 }
